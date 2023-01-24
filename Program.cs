e35298e00eeb254aab537fa9b160dc0801798fc3
nirzaf <mfmfazrin1986@gmail.com>
2023-01-24 23:08:55 +0300

    static string GetSinceTime() {     DateTime currentUtcTime = DateTime.UtcNow.ToUniversalTime().AddDays(-1);     long unixTimestamp = new DateTimeOffset(currentUtcTime).ToUnixTimeSeconds();     return unixTimestamp.ToString(); }

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+Console.WriteLine(GetSinceTime());
+
+Console.WriteLine(UnixTimeStampToDateTime(Convert.ToInt64(GetSinceTime())));
+
+Console.ReadLine();
+
+static string GetSinceTime()
+{
+    DateTime currentUtcTime = DateTime.UtcNow.ToUniversalTime().AddDays(-1);
+    long unixTimestamp = new DateTimeOffset(currentUtcTime).ToUnixTimeSeconds();
+    return unixTimestamp.ToString();
+}
+
+//convert unix timestamp to datetime
+static DateTime UnixTimeStampToDateTime(double unixTimeStamp)
+{
+    // Unix timestamp is seconds past epoch
+    DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+    dtDateTime = dtDateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+    return dtDateTime;
+}
